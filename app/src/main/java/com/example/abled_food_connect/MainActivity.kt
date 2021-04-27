92cada0a1836ba6d97c9c4e4be0713404808c0c6
sapsalgea <git config --global user.email you@example.com>
2021-04-27 14:39:54 +0900

    First Commint

---
+++ app/src/main/java/com/example/abled_food_connect/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.abled_food_connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Create 했습니다.
+    }
+}
