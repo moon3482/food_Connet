08349c08b22a98164d0b1e9cfecc1ee4f4b8acbf
moon3482 <moon3482@naver.com>
2021-05-12 18:42:59 +0900

    facebook Login

--- app/src/main/java/com/example/abled_food_connect/FacebookActivity.kt
+++ app/src/main/java/com/example/abled_food_connect/FacebookActivity.kt
@@ -2,18 +2,10 @@ package com.example.abled_food_connect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import io.socket.client.IO
-import io.socket.client.Socket
-import java.net.URISyntaxException
 
-class ChatActivity : AppCompatActivity() {
-
+class FacebookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
-
-
+        setContentView(R.layout.activity_facebook)
     }
-
 }
--- app/src/main/java/com/example/abled_food_connect/MainActivity.kt
+++ app/src/main/java/com/example/abled_food_connect/MainActivity.kt
@@ -1,20 +1,229 @@
 package com.example.abled_food_connect
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import io.socket.client.IO
 import io.socket.client.Socket
 import io.socket.emitter.Emitter
 import java.net.URISyntaxException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    lateinit var callbackManager: CallbackManager
 
+    var id = ""
+    var firstName = ""
+    var middleName = ""
+    var lastName = ""
+    var name = ""
+    var picture = ""
+    var email = ""
+    var accessToken = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnFacebookLogin : LinearLayout = findViewById(R.id.btnFacebookLogin)
+
+        callbackManager = CallbackManager.Factory.create()
 
 
+        if (isLoggedIn()) {
+            Log.d("LoggedIn? :", "YES")
+            // Show the Activity with the logged in user
+        } else {
+            Log.d("LoggedIn? :", "NO")
+            // Show the Home Activity
+        }
+
+//       Login Button made by Facebook
+//
+//
+//        val loginButton = findViewById<LoginButton>(R.id.login_button)
+//        loginButton.setReadPermissions(listOf("public_profile", "email"))
+//        // If you are using in a fragment, call loginButton.setFragment(this)
+//
+//        // Callback registration
+//        // If you are using in a fragment, call loginButton.setFragment(this)
+//
+//        // Callback registration
+//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+//            override fun onSuccess(loginResult: LoginResult?) {
+//                // Get User's Info
+//            }
+//
+//            override fun onCancel() {
+//                // App code
+//            }
+//
+//            override fun onError(exception: FacebookException) {
+//                // App code
+//            }
+//        })
+
+//      Custom Login Button
+
+
+        btnFacebookLogin.setOnClickListener {
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this, listOf("public_profile", "email"))
+        }
+
+        // Callback registration
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    Log.d("TAG", "Success Login")
+                    getUserProfile(loginResult?.accessToken, loginResult?.accessToken?.userId)
+                }
+
+                override fun onCancel() {
+                    Toast.makeText(this@MainActivity, "Login Cancelled", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: FacebookException) {
+                    Toast.makeText(this@MainActivity, exception.message, Toast.LENGTH_LONG).show()
+                }
+            })
     }
+
+    @SuppressLint("LongLogTag")
+    fun getUserProfile(token: AccessToken?, userId: String?) {
+
+        val parameters = Bundle()
+        parameters.putString(
+            "fields",
+            "id, first_name, middle_name, last_name, name, picture, email"
+        )
+        GraphRequest(token,
+            "/$userId/",
+            parameters,
+            HttpMethod.GET,
+            GraphRequest.Callback { response ->
+                val jsonObject = response.jsonObject
+
+                // Facebook Access Token
+                // You can see Access Token only in Debug mode.
+                // You can't see it in Logcat using Log.d, Facebook did that to avoid leaking user's access token.
+                if (BuildConfig.DEBUG) {
+                    FacebookSdk.setIsDebugEnabled(true)
+                    FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS)
+                }
+                accessToken = token.toString()
+
+                // Facebook Id
+                if (jsonObject.has("id")) {
+                    val facebookId = jsonObject.getString("id")
+                    Log.i("Facebook Id: ", facebookId.toString())
+                    id = facebookId.toString()
+                } else {
+                    Log.i("Facebook Id: ", "Not exists")
+                    id = "Not exists"
+                }
+
+
+                // Facebook First Name
+                if (jsonObject.has("first_name")) {
+                    val facebookFirstName = jsonObject.getString("first_name")
+                    Log.i("Facebook First Name: ", facebookFirstName)
+                    firstName = facebookFirstName
+                } else {
+                    Log.i("Facebook First Name: ", "Not exists")
+                    firstName = "Not exists"
+                }
+
+
+                // Facebook Middle Name
+                if (jsonObject.has("middle_name")) {
+                    val facebookMiddleName = jsonObject.getString("middle_name")
+                    Log.i("Facebook Middle Name: ", facebookMiddleName)
+                    middleName = facebookMiddleName
+                } else {
+                    Log.i("Facebook Middle Name: ", "Not exists")
+                    middleName = "Not exists"
+                }
+
+
+                // Facebook Last Name
+                if (jsonObject.has("last_name")) {
+                    val facebookLastName = jsonObject.getString("last_name")
+                    Log.i("Facebook Last Name: ", facebookLastName)
+                    lastName = facebookLastName
+                } else {
+                    Log.i("Facebook Last Name: ", "Not exists")
+                    lastName = "Not exists"
+                }
+
+
+                // Facebook Name
+                if (jsonObject.has("name")) {
+                    val facebookName = jsonObject.getString("name")
+                    Log.i("Facebook Name: ", facebookName)
+                    name = facebookName
+                } else {
+                    Log.i("Facebook Name: ", "Not exists")
+                    name = "Not exists"
+                }
+
+
+                // Facebook Profile Pic URL
+                if (jsonObject.has("picture")) {
+                    val facebookPictureObject = jsonObject.getJSONObject("picture")
+                    if (facebookPictureObject.has("data")) {
+                        val facebookDataObject = facebookPictureObject.getJSONObject("data")
+                        if (facebookDataObject.has("url")) {
+                            val facebookProfilePicURL = facebookDataObject.getString("url")
+                            Log.i("Facebook Profile Pic URL: ", facebookProfilePicURL)
+                            picture = facebookProfilePicURL
+                        }
+                    }
+                } else {
+                    Log.i("Facebook Profile Pic URL: ", "Not exists")
+                    picture = "Not exists"
+                }
+
+                // Facebook Email
+                if (jsonObject.has("email")) {
+                    val facebookEmail = jsonObject.getString("email")
+                    Log.i("Facebook Email: ", facebookEmail)
+                    email = facebookEmail
+                } else {
+                    Log.i("Facebook Email: ", "Not exists")
+                    email = "Not exists"
+                }
+
+
+            }).executeAsync()
+    }
+
+    fun isLoggedIn(): Boolean {
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        return isLoggedIn
+    }
+
+
+    fun logOutUser() {
+        LoginManager.getInstance().logOut()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
 }
