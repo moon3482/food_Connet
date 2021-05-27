44c650ac63a6c1e149be3dbc0cda4f123ddb0777
sapsalgea <git config --global user.email you@example.com>
2021-05-28 02:23:03 +0900

    5.27

--- app/src/main/java/com/example/abled_food_connect/fragments/ReviewFragment.kt
+++ app/src/main/java/com/example/abled_food_connect/fragments/ReviewFragment.kt
@@ -21,6 +21,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
+
 class ReviewFragment:Fragment() {
     private val reviewFragmentListArray: ArrayList<MainFragmentItemData> = ArrayList()
     lateinit var recyclerView: RecyclerView
